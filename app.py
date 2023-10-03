23f504c84ca7c22d04a6c0df0e8f7d6edfa85e37
Rohit Gupta <93462249+Guptarohit2003@users.noreply.github.com>
2023-10-03 17:44:58 +0000

    COde Refactoring

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-import pickle
+from flask import Flask, render_template, request, jsonify
+from utils import model_predict
 
 app = Flask(__name__)
 cv = pickle.load(open("models/cv.pkl",'rb'))
@@ -10,13 +10,17 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict',methods = ['POST'])
-def predict():
-    if request.method == "POST":    
-        email = request.form.get('content')
-    tokenized_email = cv.transform([email])
-    prediction = rf.predict(tokenized_email)
-    prediction = 1 if prediction == 1 else -1
+def predict():    
+    email = request.form.get('content')
+    prediction = model_predict(email)
     return render_template('index.html', prediction = prediction, email = email)
+
+@app.route('/api/predict', methods = ['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    email = data['content']
+    prediction = model_predict(email)
+    return jsonify({'prediction': prediction, 'email': email})
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=8080 , debug = True)
